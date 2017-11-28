59346c9a92e3e045beac8b9e8f72e802ffcd5ac9
Ranran9991 <ranran9991@gmail.com>
2017-11-28 15:12:50 +0200

    eclipse files changed

--- Debug/tests/subdir.mk
+++ Debug/tests/subdir.mk
@@ -3,27 +3,38 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+CC_SRCS += \
+../tests/gtest-all.cc 
+
 CPP_SRCS += \
-../Board.cpp \
-../Game.cpp \
-../Player.cpp \
-../main.cpp 
+../tests/AllTests.cpp \
+../tests/boardTest.cpp \
+../tests/gameTest.cpp 
+
+CC_DEPS += \
+./tests/gtest-all.d 
 
 OBJS += \
-./Board.o \
-./Game.o \
-./Player.o \
-./main.o 
+./tests/AllTests.o \
+./tests/boardTest.o \
+./tests/gameTest.o \
+./tests/gtest-all.o 
 
 CPP_DEPS += \
-./Board.d \
-./Game.d \
-./Player.d \
-./main.d 
+./tests/AllTests.d \
+./tests/boardTest.d \
+./tests/gameTest.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.cpp
+tests/%.o: ../tests/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+tests/%.o: ../tests/%.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
 	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
